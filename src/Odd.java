7b7275afd8f7a68ba4c6af5d9afe5bdbd8528ec7
Kaiya Park <Kaiya.park@gmail.com>
2016-02-13 11:54:11 -0800

    I used %

---
+++ src/Odd.java
@@ -0,0 +1,19 @@
+
+public class Odd {
+	public static void main(String[] args) {
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0) {
+
+				if (i % 15 != 0) {
+					System.out.println(i);
+				}
+
+			}
+			if (i % 5 == 0) {
+				if (i % 15 != 0) {
+					System.out.println(i);
+				}
+			}
+		}
+	}
+}
